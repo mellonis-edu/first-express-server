135eb15ac3343c80800482347748f62ddcdaffe9
mellonis <mellonis@yandex.ru>
2019-12-10 16:49:52 +0500

    typo fixed

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const {
 
 const app = express();
 
-const startRouter = require('./routs/start');
-const signUpRouter = require('./routs/signUp');
-const signInRouter = require('./routs/signIn');
-const homeRouter = require('./routs/home');
-const logoutRouter = require('./routs/logout');
+const startRouter = require('./routes/start');
+const signUpRouter = require('./routes/signUp');
+const signInRouter = require('./routes/signIn');
+const homeRouter = require('./routes/home');
+const logoutRouter = require('./routes/logout');
 
 app.use('/', startRouter);
 app.use('/sign-up', signUpRouter);
@@ -19,5 +19,6 @@ app.use('/home', homeRouter);
 app.use('/logout', logoutRouter);
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server is listening on port ${PORT}... Press Ctrl+C to stop it...`);
 });
